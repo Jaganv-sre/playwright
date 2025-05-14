20be0a877516542b9cc2361b12fae17cbc4b49d9
Jaganv-sre <jaganvj1@gmail.com>
2025-05-15 04:34:03 +0530

    Update AU.spec.ts
    checking for 2 pages

--- sites/AU.spec.ts
+++ sites/AU.spec.ts
@@ -19,56 +19,6 @@ const pages = [
     h1: 'Smart Financial Decisions Made Simple'
   },
   {
-    title: 'Investing',
-    url: 'https://www.forbes.com/advisor/au/investing/',
-    h1: 'How To Invest'
-  },
-  {
-    title: 'Credit Cards',
-    url: 'https://www.forbes.com/advisor/au/credit-cards/best-credit-cards/',
-    h1: 'Our Pick Of The Best Credit Cards For Australians'
-  },
-  {
-    title: 'Loans',
-    url: 'https://www.forbes.com/advisor/au/personal-loans/best-personal-loans/',
-    h1: 'Our Pick Of The Best Personal Loans For Australians'
-  },
-  {
-    title: 'Superannuation Funds',
-    url: 'https://www.forbes.com/advisor/au/superannuation/best-default-superannuation-funds-in-australia/',
-    h1: 'Our Pick Of The Best Default Superannuation Funds In 2025'
-  },
-  {
-    title: 'Car Insurance',
-    url: 'https://www.forbes.com/advisor/au/car-insurance/best-comprehensive-car-insurance-providers/',
-    h1: 'Our Pick Of The Best Comprehensive Car Insurance Providers in Australia'
-  },
-  {
-    title: 'Health Insurance',
-    url: 'https://www.forbes.com/advisor/au/health-insurance/best-private-health-insurance-companies/',
-    h1: 'Our Pick Of The Best Private Health Insurance Providers In Australia'
-  },
-  {
-    title: 'Life Insurance',
-    url: 'https://www.forbes.com/advisor/au/life-insurance/best-life-insurance-australia/',
-    h1: 'Our Pick Of The Best Life Insurance Providers For Australians'
-  },
-  {
-    title: 'Pet Insurance',
-    url: 'https://www.forbes.com/advisor/au/pet-insurance/best-pet-insurance-policies-in-australia/',
-    h1: 'Our Pick Of The Best Comprehensive Pet Insurance Policies In Australia'
-  },
-  {
-    title: 'Travel Insurance',
-    url: 'https://www.forbes.com/advisor/au/travel-insurance/best-comprehensive-travel-insurance/',
-    h1: 'Our Pick Of The Best Comprehensive Travel Insurance Providers In Australia'
-  },
-  {
-    title: 'Cryptocurrency',
-    url: 'https://www.forbes.com/advisor/au/investing/cryptocurrency/',
-    h1: 'Investing In Cryptocurrency'
-  },
-  {
     title: 'Savings Accounts',
     url: 'https://www.forbes.com/advisor/au/savings/best-high-interest-savings-accounts/',
     h1: 'Our Pick Of The Best High-Interest Savings Accounts In Australia'
